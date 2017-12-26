a6b730e3cddd410edca10ab9f9f7cd4d7ceb19c1
weaving <tanjibo@zhuozhenxuan.com>
2017-12-26 11:08:19 +0800

    修复bug

---
+++ src/MinilrssServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+namespace Tanjibo\Mini;
+
+use Illuminate\Support\ServiceProvider;
+use Tanjibo\Minilrss\Minilrss;
+
+class MiniServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $config_file = __DIR__ . '/../config/config.php';
+
+        $this->mergeConfigFrom($config_file, 'Minilrss');
+
+        $this->publishes([
+            $config_file => config_path('minilrss.php')
+        ], 'minilrss');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('minilrss', function ()
+        {
+            return new Minilrss();
+        });
+
+        $this->app->alias('minilrss', Minilrss::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['minilrss', Minilrss::class];
+    }
+}
